a0641bb4d633fd95de3d4cedbc725b9278a4d8e9
collin.lesko <lesko1cj@alma.edu>
2020-01-16 12:41:11 -0500

    copy and chmod repo files for user

--- scripts/createUsers.sh
+++ scripts/createUsers.sh
@@ -7,7 +7,7 @@
 #
 # Steps:
 # - execute script to create users, where the home directory already holds all necessary files
-# 
+#
 # Example:
 # - to create 5 users with password ACMpart2018
 #
@@ -23,45 +23,27 @@
 
 if [ -z $1 ]
 then
-    echo "Please provide a password as a second parameter"
+    echo "Please provide a user as a first parameter"
     echo ""
     echo "$ ./createUsers.sh ACMpart2018"
     exit 1
 fi
 
-if [ -z $2 ]
-then
-    echo "Please provide the username base as a third parameter"
-    echo ""
-    echo "$ ./createUsers.sh ACMpart2018 acllab"
-    exit 1
-fi
+ base=$1
 
-# numberofusers=$1
- password=$1
- base=$2
-
-#i=1
-#while [ $i -le $numberofusers ]
-#do
-	echo "ubuntu user file update"
-#	sudo useradd -m -s /bin/bash $base$i
-    sudo cp -r /etc/skel/. /home/ubuntu/.
-    sudo chmod +x /home/$base/forkGitHubRepositories.sh 
-    sudo chmod +x /home/$base/defineCredentials.sh
-    sudo chmod +x /home/$base/installAnsible.sh
-    sudo chmod +x /home/$base/installDockerRegistry.sh
-    sudo chmod +x /home/$base/installJenkins.sh
-    sudo chmod +x /home/$base/applyK8sConfig.sh
-    sudo chmod +x /home/$base/installIstio.sh
-    sudo chmod +x /home/$base/add-to-cart.sh
-    sudo chmod +x /home/$base/installKeptnBridge.sh
-    sudo chmod +x /home/$base/installPrometheusVirtualService.sh
-    sudo chmod +x /home/$base/installDynatraceServiceForKeptn.sh
-    sudo chmod +x /home/$base/deployDynatraceOperator.sh
-    sudo chmod +x /home/$base/fixProduction.sh
-    sudo chmod +x -R /home/$base/keptn/
-    sudo usermod -aG docker $base
-	echo $base:$password | sudo chpasswd
-#	i=$((i+1))
-#done
+sudo cp -r /etc/skel/. /home/$base/
+sudo chmod +x /home/$base/forkGitHubRepositories.sh
+sudo chmod +x /home/$base/defineCredentials.sh
+sudo chmod +x /home/$base/installAnsible.sh
+sudo chmod +x /home/$base/installDockerRegistry.sh
+sudo chmod +x /home/$base/installJenkins.sh
+sudo chmod +x /home/$base/applyK8sConfig.sh
+sudo chmod +x /home/$base/installIstio.sh
+sudo chmod +x /home/$base/add-to-cart.sh
+sudo chmod +x /home/$base/installKeptnBridge.sh
+sudo chmod +x /home/$base/installPrometheusVirtualService.sh
+sudo chmod +x /home/$base/installDynatraceServiceForKeptn.sh
+sudo chmod +x /home/$base/deployDynatraceOperator.sh
+sudo chmod +x /home/$base/fixProduction.sh
+sudo chmod +x -R /home/$base/keptn/
+sudo usermod -aG docker $base
